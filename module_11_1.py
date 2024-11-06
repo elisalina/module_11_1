cf4ffdfec3c4536f91edd5e352ec4707e8614e39
Алина Демчук <alina2805@bk.ru>
2024-11-06 14:49:57 +0300

    Add files via upload

---
+++ module_11_1.py
@@ -0,0 +1,16 @@
+import numpy as np
+#import pandas as pd
+#import requests
+
+#image = requests.get('https://learn.python.ru/media/projects/sl1_Cj4bKxp.png')
+#with open('diagramma.png', 'wb') as file:
+#    file.write(image.content)
+
+#r = pd.read_csv('test_text.txt')
+#print(r)
+
+a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(a)
+print(a + 2)
+print(a * 2)
+print(a < 4)
